8b8dcd638ce75e3209026bcacedbd56d4d0b8293
nrusso21 <nrusso@protonmail.com>
2020-01-08 13:40:08 -0500

    Create simple redux store

---
+++ src/lib/redux.js
@@ -0,0 +1,32 @@
+import { createStore } from 'redux';
+
+//  A simple redux store/actions/reducer implementation.
+// A true app would be more complex and separated into different files.
+
+export const actions = {
+    ARCHIVE_TASK: 'ARCHIVE_TASK',
+    PIN_TASK: 'PIN_TASK',
+};
+
+export const archiveTask = id => ({ type: actions.ARCHIVE_TASK, id });
+export const pinTask = id => ({ type:actions.PIN_TASK, id });
+
+function taskStateReducer(taskState) {
+    return (state, action) => {
+        return {
+            ...state,
+            tasks: state.tasks.map(task =>
+                task.id === action.id ? { ...task, state: taskState } : task
+            ),
+        };
+    };
+}
+
+const defaultTasks = [
+    { id: '1', title: 'Something', state: 'TASK_INBOX'},
+    { id: '2', title: 'Something more', state: 'TASK_INBOX'},
+    { id: '3', title: 'Something else', state: 'TASK_INBOX'},
+    { id: '4', title: 'Something again', state: 'TASK_INBOX'},
+];
+
+export default createStore(reducer, { tasks: defaultTasks });
